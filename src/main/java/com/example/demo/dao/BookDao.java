c8845e3b3da5d3db640f6e473cb4bc53f2950a02
ZhenyaGleen <evgeniy.goncharov.98@inbox.ru>
2020-09-25 15:51:06 +0300

    Initial commit

---
+++ src/main/java/com/example/demo/dao/BookDao.java
@@ -0,0 +1,18 @@
+package com.example.demo.dao;
+
+
+import com.example.demo.model.Book;
+
+import java.util.List;
+
+public interface BookDao {
+    public void addBook(Book book);
+
+    public void updateBook(Book book);
+
+    public void removeBook(int id);
+
+    public Book getBookById(int id);
+
+    public List<Book> listBooks();
+}
---
+++ src/main/java/com/example/demo/service/BookService.java
@@ -0,0 +1,17 @@
+package com.example.demo.service;
+
+import com.example.demo.model.Book;
+
+import java.util.List;
+
+public interface BookService {
+    public void addBook(Book book);
+
+    public void updateBook(Book book);
+
+    public void removeBook(int id);
+
+    public Book getBookById(int id);
+
+    public List<Book> listBooks();
+}
